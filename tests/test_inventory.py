55a3bf842c1b4b67ce44dcc19db1dafa98321aa6
wbssr <3157484161@qq.com>
2026-09-17 21:39:49 +0800

    continue to Solve unstable overseas servers.

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -20,6 +20,7 @@ class TestInventory:
         inventory = InventoryPage(driver)
         assert inventory.is_loaded() == True
 
+    @pytest.mark.flaky
     @pytest.mark.regression
     def test_add_two_products(self, driver):
         inventory = InventoryPage(driver)
